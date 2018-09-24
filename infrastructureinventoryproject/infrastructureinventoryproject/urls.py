67a51bb631667d77b7ee0782059a8b4ad35bf3c2
Raffi Bilemjian <rbilemjian@gmail.com>
2018-09-24 09:46:05 -0700

    Added Login Page and Routing with Basic Functionality
    TODO: Add user registration & logout functionalities

--- infrastructureinventoryproject/infrastructureinventoryproject/urls.py
+++ infrastructureinventoryproject/infrastructureinventoryproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from infrastructureinventoryproject import views
 
 urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
     url(r'^infrastructureinventory/', include('infrastructureinventoryapp.urls')),
 ]
