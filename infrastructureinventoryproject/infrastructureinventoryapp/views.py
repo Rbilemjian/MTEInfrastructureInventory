8f231dd07eb914b530c523c6ab875ec1463bbc7a
Raffi Bilemjian <rbilemjian@gmail.com>
2018-09-25 10:57:10 -0700

    Added static image files, now displaying app servers from DB in a table
    TODO: Allow user to import data to database from excel file, and create form for user to input data within the website

--- infrastructureinventoryproject/infrastructureinventoryapp/views.py
+++ infrastructureinventoryproject/infrastructureinventoryapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import ApplicationServer
 
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    applicationServers = ApplicationServer.objects.all()
+    return render(request, 'home.html', {'applicationServers': applicationServers})
