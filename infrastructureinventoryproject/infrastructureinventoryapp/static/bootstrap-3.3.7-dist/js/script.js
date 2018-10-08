30bf914add797f4734360a13fef01ec0da92f850
Raffi Bilemjian <rbilemjian@gmail.com>
2018-10-08 13:11:10 -0700

    Fixed export to account for not having an edit column anymore

--- infrastructureinventoryproject/infrastructureinventoryapp/static/bootstrap-3.3.7-dist/js/script.js
+++ infrastructureinventoryproject/infrastructureinventoryapp/static/bootstrap-3.3.7-dist/js/script.js
@@ -9,32 +9,31 @@ $(document).ready(function() {
         select: true,
 
 
-        
-        //export datatable columns not including the "edit" column
+        //export datatable columns
         dom: 'Bfrtip',
         buttons: [
             {
                 extend: 'copy',
                 exportOptions: {
-                    columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
                 }
             },
             {
                 extend: 'csv',
                 exportOptions: {
-                    columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
                 }
             },
             {
                 extend: 'excel',
                 exportOptions: {
-                    columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
                 }
             },
             {
                 extend: 'pdf',
                 exportOptions: {
-                    columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
                 }
             }
     ], 
