6209b8ad1d04772e8af33b19bb0580a2702bf435
Raffi Bilemjian <rbilemjian@gmail.com>
2018-10-16 08:32:51 -0700

    Added all missing columns to table export, changed color based on hwdate
    Columns were added to the datatable to make it more complete a while ago, but the export definitions did not reflect the additional columns. Now they do. The color of each entry can also now be yellow or red depending on the proximity of the next hardware support date. If the date is within the next month, the table entry will be yellow. If it has already passed, the table entry will be red.

--- infrastructureinventoryproject/infrastructureinventoryapp/static/bootstrap-3.3.7-dist/js/script.js
+++ infrastructureinventoryproject/infrastructureinventoryapp/static/bootstrap-3.3.7-dist/js/script.js
@@ -13,28 +13,28 @@ $(document).ready(function() {
             {
                 extend: 'copy',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
                 }
             },
             {
                 extend: 'csv',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
                 }
             },
             {
                 extend: 'excel',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
                 }
             },
             {
                 extend: 'pdf',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
                 }
             }
-    ], 
+        ],
         
         
 // individual column searching
@@ -58,6 +58,24 @@ $(document).ready(function() {
                 } );
             } );
         },
+
+        "createdRow": function(row, data, dataIndex)
+        {
+            var hwDate = data[22];
+            if(hwDate != "None")
+            {
+                var hwDateMilliseconds = Date.parse(hwDate);
+                var todayMilliseconds = Date.parse(new Date());
+                var difference = hwDateMilliseconds - todayMilliseconds;
+//                alert(difference);
+                if(difference <= 0)
+                    $(row).addClass('red');
+                else if(difference < 2592000000)
+                    $(row).addClass('yellow');
+
+            }
+        },
+
     });
 });
 
