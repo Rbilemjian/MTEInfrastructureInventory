099a53a4a8974c293586f9b4c6b7ed16ef43dae4
Raffi Bilemjian <rbilemjian@gmail.com>
2018-12-12 12:12:13 -0800

    Cleaned up Unnecessary Comments

--- infrastructureinventoryproject/infrastructureinventoryapp/urls.py
+++ infrastructureinventoryproject/infrastructureinventoryapp/urls.py
@@ -8,30 +8,24 @@ urlpatterns = [
     url(r'^login/$', login, {'template_name': 'login.html'}),
     url(r'^logout/$', logout, {'template_name': 'logout.html', 'next_page': '/infrastructureinventory/login'}),
 
+
     #information viewing urls
     url(r'^applicationserver/$', views.view_application_servers),
     url(r'^applicationserver/details/(?P<pk>\d+)/$', views.details_application_server, name="details-view"),
-    #
-    # #form urls
-    # url(r'^applicationserver/add/$', views.create_application_server_form),
-    # url(r'^applicationserver/edit/(?P<pk>\d+)/$', views.edit_application_server, name="edit-view"),
-    #
-    # #import urls
-    # url(r'^applicationserver/import/$', views.import_application_server),
-    # url(r'^applicationserver/import/confirm/$', views.confirm_import_application_server),
-    #
+
+
     # #delete urls
     url(r'^applicationserver/delete/confirm/(?P<pk>\d+)/$', views.application_server_delete_confirm, name="delete-confirmation-view"),
     url(r'^applicationserver/delete/(?P<pk>\d+)/$', views.delete_application_server, name="delete-view"),
-
     url(r'^applicationserver/filterprofile/delete/confirm/(?P<pk>\d+)/$', views.filter_profile_delete_confirm, name="filter-profile-delete-confirm-view"),
     url(r'^applicationserver/filterprofile/delete/(?P<pk>\d+)/$', views.filter_profile_delete, name="filter-profile-delete"),
-
     url(r'^applicationserver/batchdelete/confirm', views.confirm_batch_delete, name='batch-delete-confirm'),
-    #
+
+
     # #search urls
     url(r'^applicationserver/search', views.search_application_server, name="search-view"),
-    #
+
+
     # #filter profile urls
     url(r'^applicationserver/filteredlist/(?P<pk>\d+)/$', views.filtered_list, name="filtered-list-view"),
     url(r'^applicationserver/filterprofile/$', views.filter_profile, name="filter-profile-view"),
--- infrastructureinventoryproject/infrastructureinventoryapp/views.py
+++ infrastructureinventoryproject/infrastructureinventoryapp/views.py
@@ -2,88 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 
 from django.http import HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
-from django.utils import timezone
 from . import models
-from .models import ApplicationServer, FilterProfile, HOST_FIELDS, DHCPMember, CloudInformation, DISCOVERED_DATA_FIELDS
+from .models import ApplicationServer, FilterProfile, DHCPMember, CloudInformation
 from .models import SNMP3Credential, SNMPCredential, ExtensibleAttribute, DiscoveredData, IPv4HostAddress
 from .models import IPv6HostAddress, DomainNameServer, LogicFilterRule, Alias, CliCredential, DHCPOption, VisibleColumns
-from .models import HOST_FIELDS, AWSRTE53RecordInfo
-from .forms import InfobloxImportForm, VisibleColumnForm, FilterProfileForm, AdvancedSearchForm
-import xlrd
-import requests
+from .models import AWSRTE53RecordInfo
+from .forms import VisibleColumnForm, FilterProfileForm, AdvancedSearchForm
 import json
-import urllib3
 
-#helper functions
-
-
-# def get_str_date(row, column, worksheet, book):
-#     date_tuple = xlrd.xldate.xldate_as_tuple(worksheet.cell_value(row, column), book.datemode)
-#     return str(date_tuple[0]) + "-" + str(date_tuple[1]) + "-" + str(date_tuple[2])
-#
-#
-# def sanitize_server(server, request):
-#     #correcting fields
-#     fields = ApplicationServer._meta.get_all_field_names()
-#     for field in fields:
-#         if hasattr(server, field):
-#             if getattr(server, field) == "":
-#                     setattr(server, field, None)
-#     if server.is_virtual_machine == "TBD":
-#         server.is_virtual_machine = None
-#     if server.environment == "TBD":
-#         server.environment = None
-#     server.published_by = request.user
-#     server.published_date = timezone.now()
-#
-#     #checking if exists already in database
-#     if ApplicationServer.objects.filter(
-#         service=server.service,
-#         hostname=server.hostname,
-#         primary_application=server.primary_application,
-#         is_virtual_machine=server.is_virtual_machine,
-#         environment=server.environment,
-#         location=server.location,
-#         data_center=server.data_center,
-#         operating_system=server.operating_system,
-#         rack=server.rack,
-#         model=server.model,
-#         serial_number=server.serial_number,
-#         comment=server.comment,
-#         network=server.network,
-#         private_ip=server.private_ip,
-#         dmz_public_ip=server.dmz_public_ip,
-#         virtual_ip=server.virtual_ip,
-#         nat_ip=server.nat_ip,
-#         ilo_or_cimc=server.ilo_or_cimc,
-#         nic_mac_address=server.nic_mac_address,
-#         switch=server.switch,
-#         port=server.port,
-#         base_warranty=server.base_warranty,
-#     ).exists():
-#         return {'exists_in_database': True, 'server': server}
-#     return {'exists_in_database': False, 'server': server}
-#
-#
-# def update_server(server, request):
-#     fields = ApplicationServer._meta.get_all_field_names()
-#     for field in fields:
-#         if hasattr(server, field):
-#             if getattr(server, field) == "":
-#                     setattr(server, field, None)
-#     if server.is_virtual_machine == "TBD":
-#         server.is_virtual_machine = 0
-#     if server.environment == "TBD":
-#         server.environment = "Prod"
-#     server.last_edited = timezone.now()
-#     server.last_editor = request.user
-#     server.save()
-#     return server
-#
-#
+#-------Filtering Helper Functions--------
 
 #If a user searches for an ipv4address, this function looks at ipv4hostaddresses and finds the applicationserver
-#that is associated with the ivp4hostaddress that has that IP address, if such an appliationServer exists
+#that is associated with the ivp4hostaddress that has that IP address, if such an applicationServer exists
 #as well as any record applicationservers that have that ip address
 def get_ipv4_application_servers(ipv4addr):
     ipv4s = IPv4HostAddress.objects.filter(visible=True)
@@ -482,221 +412,6 @@ def filter_servers(filterProfile):
                 search_result = search_result.filter(**{filter: value})
     search_result = search_result.filter(visible=True)
     return search_result
-#
-#
-# def filter_from_profile(filter_profile):
-#     filter_result = ApplicationServer.objects.all()
-#     if filter_profile.all_fields is not None:
-#         fields = filter_profile._meta.get_all_field_names()
-#         results = ApplicationServer.objects.none()
-#         for field in fields:
-#             if field == "user_id" or field == "user" or field == "profile_name" or field == "all_fields":
-#                 continue
-#             lookup = "%s__icontains" % field
-#             query = {lookup: filter_profile.all_fields}
-#             results = results | ApplicationServer.objects.filter(**query)
-#         filter_result = results
-#
-#
-#     if filter_profile.service is not None:
-#         filter_result = filter_result.filter(service__icontains=filter_profile.service)
-#
-#     if filter_profile.hostname is not None:
-#         filter_result = filter_result.filter(hostname__icontains=filter_profile.hostname)
-#
-#     if filter_profile.primary_application is not None:
-#         filter_result = filter_result.filter(primary_application__icontains=filter_profile.primary_application)
-#
-#     if filter_profile.is_virtual_machine is not None:
-#         filter_result = filter_result.filter(is_virtual_machine=filter_profile.is_virtual_machine)
-#
-#     if filter_profile.environment is not None:
-#         filter_result = filter_result.filter(environment=filter_profile.environment)
-#
-#     if filter_profile.location is not None:
-#         filter_result = filter_result.filter(location__icontains=filter_profile.location)
-#
-#     if filter_profile.data_center is not None:
-#         filter_result = filter_result.filter(data_center__icontains=filter_profile.data_center)
-#
-#     if filter_profile.operating_system is not None:
-#         filter_result = filter_result.filter(operating_system__icontains=filter_profile.operating_system)
-#
-#     if filter_profile.rack is not None:
-#         filter_result = filter_result.filter(rack__icontains=filter_profile.rack)
-#
-#     if filter_profile.model is not None:
-#         filter_result = filter_result.filter(model__icontains=filter_profile.model)
-#
-#     if filter_profile.serial_number is not None:
-#         filter_result = filter_result.filter(serial_number__icontains=filter_profile.serial_number)
-#
-#     if filter_profile.network is not None:
-#         filter_result = filter_result.filter(network__icontains=filter_profile.network)
-#
-#     if filter_profile.private_ip is not None:
-#         filter_result = filter_result.filter(private_ip__icontains=filter_profile.private_ip)
-#
-#     if filter_profile.dmz_public_ip is not None:
-#         filter_result = filter_result.filter(dmz_public_ip__icontains=filter_profile.dmz_public_ip)
-#
-#     if filter_profile.virtual_ip is not None:
-#         filter_result = filter_result.filter(virtual_ip__icontains=filter_profile.virtual_ip)
-#
-#     if filter_profile.nat_ip is not None:
-#         filter_result = filter_result.filter(nat_ip__icontains=filter_profile.nat_ip)
-#
-#     if filter_profile.ilo_or_cimc is not None:
-#         filter_result = filter_result.filter(ilo_or_cimc__icontains=filter_profile.ilo_or_cimc)
-#
-#     if filter_profile.nic_mac_address is not None:
-#         filter_result = filter_result.filter(nic_mac_address__icontains=filter_profile.nic_mac_address)
-#
-#     if filter_profile.switch is not None:
-#         filter_result = filter_result.filter(switch__icontains=filter_profile.switch)
-#
-#     if filter_profile.port is not None:
-#         filter_result = filter_result.filter(port__icontains=filter_profile.port)
-#
-#     if filter_profile.purchase_order is not None:
-#         filter_result = filter_result.filter(purchase_order__icontains=filter_profile.purchase_order)
-#
-#
-#     return filter_result
-#
-#
-
-#
-#
-# @login_required
-# def create_application_server_form(request):
-#     if request.method == 'POST':
-#         form = ServerForm(request.POST)
-#         if form.is_valid():
-#             server = form.save(commit=False)
-#             sanitization_result = sanitize_server(server, request)
-#             server = sanitization_result['server']
-#             server.save()
-#             return redirect('/infrastructureinventory/applicationserver/')
-#     else:
-#         form = ServerForm()
-#     # if form invalid or GET request
-#     return render(request, 'application_server_form.html', {"form": form})
-#
-#
-# @login_required
-# def import_application_server(request):
-#     if request.method == 'POST':
-#         form = ServerImportForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             ApplicationServer.objects.filter(visible=False).delete()
-#             file = request.FILES['file']
-#             book = xlrd.open_workbook(file_contents=file.read())
-#             worksheet = book.sheet_by_name('Sheet1')
-#             num_rows = worksheet.nrows - 1
-#             for i in range(1, num_rows + 1):
-#
-#                 app_server = ApplicationServer()
-#
-#                 app_server.service = worksheet.cell_value(i, 0).strip()
-#                 app_server.primary_application = worksheet.cell_value(i, 1).strip()
-#                 app_server.hostname = worksheet.cell_value(i, 2).strip()
-#                 app_server.is_virtual_machine = worksheet.cell_value(i, 3)
-#                 app_server.environment = worksheet.cell_value(i, 4).strip()
-#                 app_server.location = worksheet.cell_value(i, 5).strip()
-#                 app_server.data_center = str(worksheet.cell_value(i, 6)).strip()
-#                 app_server.operating_system = worksheet.cell_value(i, 8).strip()
-#                 app_server.model = worksheet.cell_value(i, 9).strip()
-#                 app_server.serial_number = worksheet.cell_value(i, 10).strip()
-#                 app_server.network = worksheet.cell_value(i, 11).strip()
-#
-#                 if type(worksheet.cell_value(i, 7)) is float:
-#                     app_server.rack = str(int(worksheet.cell_value(i, 7)))
-#                 else:
-#                     app_server.rack = str(worksheet.cell_value(i, 7)).strip()
-#
-#                 app_server.private_ip = worksheet.cell_value(i, 12)
-#                 app_server.dmz_public_ip = worksheet.cell_value(i, 13)
-#                 app_server.virtual_ip = worksheet.cell_value(i, 14)
-#                 app_server.nat_ip = worksheet.cell_value(i, 15)
-#                 app_server.ilo_or_cimc = worksheet.cell_value(i, 16).strip()
-#                 app_server.nic_mac_address = worksheet.cell_value(i, 17).strip()
-#                 app_server.switch = worksheet.cell_value(i, 18).strip()
-#                 app_server.port = worksheet.cell_value(i, 19).strip()
-#
-#                 if type(worksheet.cell_value(i, 20)) is float:
-#                     app_server.purchase_order = str(int(worksheet.cell_value(i, 20)))
-#                 else:
-#                     app_server.purchase_order = worksheet.cell_value(i, 20)
-#
-#                 if worksheet.cell_value(i, 21) == "":
-#                     app_server.start_date = None
-#                 else:
-#                     app_server.start_date = get_str_date(i, 21, worksheet, book)
-#
-#                 if worksheet.cell_value(i, 22) == "":
-#                     app_server.next_hardware_support_date = None
-#                 else:
-#                     app_server.next_hardware_support_date = get_str_date(i, 22, worksheet, book)
-#
-#                 if worksheet.cell_value(i, 23) == "":
-#                     app_server.base_warranty = None
-#                 else:
-#                     app_server.base_warranty = get_str_date(i, 23, worksheet, book)
-#
-#                 app_server.cpu = worksheet.cell_value(i, 24)
-#                 app_server.ram = worksheet.cell_value(i, 25)
-#                 app_server.c_drive = worksheet.cell_value(i, 26)
-#                 app_server.d_drive = worksheet.cell_value(i, 27)
-#                 app_server.e_drive = worksheet.cell_value(i, 28)
-#
-#                 if worksheet.cell_value(i, 29) == "":
-#                     app_server.comment = None
-#                 else:
-#                     app_server.comment = worksheet.cell_value(i, 29)
-#
-#                 sanitization_result = sanitize_server(app_server, request)
-#                 if sanitization_result['exists_in_database'] is False:
-#                     app_server = sanitization_result['server']
-#                     app_server.visible = False
-#                     app_server.save()
-#
-#             return redirect('/infrastructureinventory/applicationserver/import/confirm')
-#
-#             #return redirect('/infrastructureinventory/applicationserver')
-#     else:
-#         form = ServerImportForm()
-#     # if form invalid or GET request
-#     return render(request, 'application_server_import.html', {"form": form})
-#
-# @login_required()
-# def confirm_import_application_server(request):
-#     parsed_servers = ApplicationServer.objects.filter(visible=False)
-#     if request.method == "POST":
-#         for ps in parsed_servers:
-#             ps.visible=True
-#             ps.save()
-#         return redirect('/infrastructureinventory/applicationserver')
-#     else:
-#         return render(request, 'application_server_import_confirm.html', {"applicationServers": parsed_servers})
-#
-#
-#
-# @login_required()
-# def edit_application_server(request, pk):
-#     applicationServer = get_object_or_404(ApplicationServer, pk=pk)
-#     if request.method == "POST":
-#         form = ServerForm(request.POST, instance=applicationServer)
-#         if form.is_valid():
-#             server = form.save(commit=False)
-#             update_server(server, request)
-#             return redirect('details-view', pk=server.pk)
-#     else:
-#         form = ServerForm(instance=applicationServer)
-#
-#     #if form is invalid or GET request
-#     args = {'form': form}
-#     return render(request, 'application_server_edit.html', args)
 
 
 def prep_filter_for_save(filter):
@@ -707,6 +422,10 @@ def prep_filter_for_save(filter):
             if FilterProfile._meta.get_field(field).null:
                 setattr(filter, field, None)
     return filter
+
+
+#------Visible Field Helper Functions------
+
 
 def get_visible_fields(request):
     visible_columns = VisibleColumns.objects.filter(user=request.user).get()
@@ -793,8 +512,8 @@ def get_visible_discovered_data_fields(request):
                 retList.append(field)
     return retList
 
-#View Functions
 
+#------View Functions-------
 
 
 @login_required
